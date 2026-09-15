2b7e969f69861e070fd3ce72e1c4b845e2e132bc
Shivani <kakatkarshivani@gmail.com>
2026-09-15 22:26:28 +0530

    Add new updates to springcrud module

--- SpringCRUD/src/main/java/com/springcrud/service/StudentService.java
+++ SpringCRUD/src/main/java/com/springcrud/service/StudentService.java
@@ -4,6 +4,8 @@ import com.springcrud.entity.Student;
 import com.springcrud.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class StudentService {
     private StudentRepository studentRepository;
@@ -14,8 +16,15 @@ public class StudentService {
 
     public Student createStudent(Student studentReq){
         System.out.println("Inside student service");
-        Student studentResp = studentRepository.saveStudent(studentReq);
+        Student studentResp = studentRepository.save(studentReq);
         System.out.println("Exiting student service");
         return studentResp;
+    }
+    public Student getStudent(Long id){
+        Optional<Student> studentResp = studentRepository.findById(id);
+        if (studentResp.isPresent()){
+            return studentResp.get();
+        }
+        return null;
     }
 }
